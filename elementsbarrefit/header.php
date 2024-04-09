cf424471ef6e89cf5f46fb3bfb661a51928f4bef
calcucode <162299184+calcucode@users.noreply.github.com>
2024-04-09 09:47:08 +0700

    Create header.php

---
+++ elementsbarrefit/header.php
@@ -0,0 +1,212 @@
+<!doctype html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+<meta name="facebook-domain-verification" content="g2kinsoezusijnlsy3021splhdve8m" />
+<meta name="google-site-verification" content="C1ggUvI7rWjFR3k83vATViBBCTe7iCKu6eWjDc7TPEI" />
+<meta name="apple-itunes-app" content="app-id=1526147810, app-argument=https://apps.apple.com/us/app/elements-barre-fit/id1526147810">
+
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5239921618837213"
+     crossorigin="anonymous"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5239921618837213"
+     crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/cc2e2ffac6.js" crossorigin="anonymous"></script>
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Elements Barre Fit",
+  "alternateName": "elementsbarrefit",
+  "url": "https://elementsbarrefit.com/",
+  "logo": "https://elementsbarrefit.com/wp-content/uploads/2021/09/Elements-Barre-Fit-Logo-e1631547101322.png",
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "1 (888) 572-6217",
+    "contactType": "customer service",
+    "areaServed": "US",
+    "availableLanguage": "en"
+  },
+  "sameAs": [
+    "https://www.instagram.com/elementsbarrefit/",
+    "https://www.facebook.com/elementsbarrefit/",
+    "https://twitter.com/elementsfitnyc",
+    "https://www.linkedin.com/company/elements-fitness-studio",
+    "https://www.youtube.com/channel/UCs6FPRZ6O0HvtoLYwJ7_NSg"
+  ]
+}
+</script>
+
+<!-- Facebook Pixel Code -->
+<script>
+setTimeout(function(){
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2957315764375297');
+fbq('track', 'PageView');
+}, 3500);
+</script>
+
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5239921618837213"
+     crossorigin="anonymous"></script>
+
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=2957315764375297&ev=PageView&noscript=1"/>
+</noscript>
+
+
+<!-- End Facebook Pixel Code -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script defer src="https://www.googletagmanager.com/gtag/js?id=UA-179088836-1"></script>
+<!-- <script defer src="https://elementsbarrefit.com/wp-content/themes/elements-ipstudio/js/scripts.js"></script> -->
+   
+<!-- End Global site tag (gtag.js) -->
+   <meta charset="<?php bloginfo( 'charset' ); ?>">
+   <!--<meta name="viewport" content="width=device-width, initial-scale=1" >-->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+   
+   <link rel="profile" href="https://gmpg.org/xfn/11">
+   <?php wp_head(); ?>
+   
+   
+
+</head>
+
+
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5239921618837213"
+     crossorigin="anonymous"></script>
+
+
+<body <?php body_class(); ?>>
+    <?php //wp_body_open(); ?>
+   
+   
+   <div id="page" class="site">
+<div class="complete_header"> 
+   <div class="studio-banner covid_up">
+      <div class="container">
+           <a style="color: #fff !important; text-decoration: none;" href="https://elementsbarrefit.com/sag-harbor/">
+			<span> We're moving ! New Sag Harbor studio opening Spring 2024!
+            </span>           
+         </a>
+      </div>
+      <button class="banner-close" type="button">×</button>
+   </div>
+   
+<div class="top-header">
+      <div class="container-fluid">
+         <ul>
+
+            <li class="my-account-link"><a href="/my-account"><i class="fas fa-user-circle"></i> MY ACCOUNT</a></li>
+            <li class="nearest-location-up"><a href="#" class="popmake-7138"><i class="fas fa-map-marker-alt"></i> <strong class="loctext text-uppercase">SELECT LOCATION:</strong>  <span>(Change)</span></a></li>
+         </ul>
+      </div>
+   </div>
+
+
+
+   
+
+   
+   
+   
+   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5239921618837213"
+     crossorigin="anonymous"></script>
+   
+   
+   
+   
+   <header id="masthead" class="site-header">
+      <div class="container-fluid">
+         <div class="row align-items-center">
+            <div class="col-md-1 col-1">
+               <div class="site-branding">
+                  <?php
+                  the_custom_logo();
+                     ?>
+               </div>                                          
+            </div>
+
+            <div class="col-md-7 col-1">              
+                   <nav id="site-navigation" class="main-navigation">
+                  <?php
+                  wp_nav_menu(
+                     array(
+                        'theme_location' => 'menu-1',
+                        'menu_id'        => 'primary-menu',
+                     )
+                  );
+                  ?>
+               </nav>                              
+            </div>            
+            
+            
+            <div class="col-md-4 col-10 header-right-holder">
+               <nav id="site-navigation" class="main-navigation">
+                  <?php
+                  wp_nav_menu(
+                     array(
+                        'theme_location' => 'menu-3',
+                        'menu_id'        => 'secondary-menu',
+                     )
+                  );
+                  ?>
+               </nav><!-- #site-navigation -->  
+                        
+               <div class="book-btn mobile-book">
+                  <a href="https://elementsbarrefit.com/book-a-service/">Book a Service</a>
+               </div>
+               <div class="mobile-hamburger">
+                  <img class="ham-menu" src="/wp-content/uploads/2021/03/bar.png" />
+                  <img class="close-menu" src="/wp-content/uploads/2020/08/clsoe.png" />
+               </div>
+            </div>
+         </div>
+      </div>
+<div class="mobile-menu-wrapper">
+   <div class="mobile-menu-container">
+      <div class="container">
+         <ul>
+            <li class="mb-1 buy-btn mobile-buy"><a class="btn-primary text-white" href=" https://elementsbarrefit.com/studio-pricing-buy-classes/purchase-classes/">BUY</a></li>
+            <li class="mb-4 mt-4"><a href="https://watch.elementsbarrefit.com/" target="_blank" class="btn-primary text-white">ON DEMAND</a></li>
+         </ul>       
+         <?php
+         wp_nav_menu(
+            array(
+               'theme_location' => 'menu-2',
+               'menu_id'        => 'mobile-menu',
+               'container' => 'ul',
+            )
+         );
+         ?>
+ 
+             <ul class="mt-4 mb-4">
+            <li><a class="underline" href="/my-account"><i class="fas fa-user-circle pink"></i> MY ACCOUNT</a></li>
+         </ul>                
+         <ul>
+                  <li><a href="/franchise/">Franchise</a></li>
+                  <li><a href="/studio-rental/">Studio Rental</a></li>
+                  <li><a href="/join-our-team/">Join our Team</a></li>
+         </ul>
+      </div>
+   </div>      
+   </div>   
+</header><!-- #masthead -->
+</div>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5239921618837213"
+     crossorigin="anonymous"></script>
